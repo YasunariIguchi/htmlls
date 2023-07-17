a89153b68a3d9031af0b5b5dbe098f27ba0e497d
YasunariIguchi <iguchi.yasunari.0219@gmail.com>
2023-07-17 12:21:08 +0900

    5.1

--- album/js/index.js
+++ album/js/index.js
@@ -1,4 +1,16 @@
 // (1) ページ本体が読み込まれたタイミングで実行するコード
 const result = document.getElementById('result');
-result.textContent = 'Hello, JavaScript!!!';
-//window.alert(result.textContent);
+if (navigator.geolocation) {
+  //現在位置を取得するコード
+  navigator.geolocation.getCurrentPosition(
+    (pos) => {
+      //現在位置の取得に成功した時の処理
+      msg = `緯度：${pos.coords.latitude}<br />
+        経度：${pos.coords.longitude}<br />
+        方角：${pos.coords.heading}`;
+      result.innerHTML = msg;
+    }
+  );
+} else {
+  window.alert('Geolocation APIに対応しているブラウザーでアクセスしてください。');
+}
